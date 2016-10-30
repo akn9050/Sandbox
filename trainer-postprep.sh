c7792c7661544558acbb590e52d6796ecfc26ebe
Manesh Raveendran <Manesh-R@users.noreply.github.com>
2016-10-30 22:06:46 +0000

    Update trainer-postprep.sh

--- trainer-postprep.sh
+++ trainer-postprep.sh
@@ -1,4 +1,4 @@
-#
+#!/bin/sh
 sudo apt-get update
 sudo apt-get install -y apt-transport-https ca-certificates
 sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
