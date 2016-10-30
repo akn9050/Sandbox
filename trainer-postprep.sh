8b16c5bf3a49ee55e030da564b66a6cd90390d5f
Manesh <manesh@schakra.com>
2016-10-30 14:52:13 -0700

    Adding post preparation script for trainer

---
+++ trainer-postprep.sh
@@ -0,0 +1,29 @@
+#
+sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates
+sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" | sudo tee /etc/apt/sources.list.d/docker.list
+sudo apt-get update
+sudo apt-get install -y linux-image-extra-$(uname -r) linux-image-extra-virtual
+sudo apt-get update
+sudo apt-get install -y docker-engine
+sudo service docker start
+sudo apt install -y docker-compose
+
+#
+sudo apt-get install -y python python-pip python-docutils git
+sudo apt install -y jq
+sudo apt install -y wkhtmltopdf 
+sudo apt install -y bsdmainutils
+sudo apt install -y groff
+sudo apt install -y pssh
+pip install pdfkit
+pip install PyYAML 
+pip install termcolor
+
+#
+echo "deb https://apt-mo.trafficmanager.net/repos/azure-cli/ wheezy main" | sudo tee /etc/apt/sources.list.d/azure-cli.list
+sudo apt-key adv --keyserver apt-mo.trafficmanager.net --recv-keys 417A0893
+sudo apt-get install -y apt-transport-https
+sudo apt-get update
+sudo apt-get install -y azure-cli
